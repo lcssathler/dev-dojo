2af42a7f380c2fcf3f8d6b1f3d0fa1a56f10923a
Lucas Sathler <lcssathler@gmail.com>
2023-05-19 17:35:16 -0300

    refactoring test...

--- src/maratonajava/javacore/Wnio/test/TestGit.java
+++ src/maratonajava/javacore/Wnio/test/TestGit.java
@@ -2,6 +2,6 @@ package maratonajava.javacore.Wnio.test;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("Git is Amazing!");
+        System.out.println("Git");
     }
 }
