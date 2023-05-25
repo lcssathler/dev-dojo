d34c9d9798054eb3c93c7b57eb73a0f7387bd077
Lucas Sathler <lcssathler@gmail.com>
2023-05-25 17:40:51 -0300

    Aula 161 - Equals  pt. 01

---
+++ src/maratonajava/javacore/Ycolecoes/domain/Smartphone.java
@@ -0,0 +1,11 @@
+package maratonajava.javacore.Ycolecoes.domain;
+
+public class Smartphone {
+    private String serial;
+    private String manufacturer;
+
+    public Smartphone(String serial, String manufacturer) {
+        this.serial = serial;
+        this.manufacturer = manufacturer;
+    }
+}
---
+++ src/maratonajava/javacore/Ycolecoes/test/EqualsTest01.java
@@ -0,0 +1,18 @@
+package maratonajava.javacore.Ycolecoes.test;
+
+import maratonajava.javacore.Ycolecoes.domain.Smartphone;
+
+public class EqualsTest01 {
+    public static void main(String[] args) {
+        String name1 = "Lucas";
+        String name2 = new String("Lucas");
+        System.out.println(name1 == name2);
+        System.out.println(name1.equals(name2));
+        System.out.println();
+
+        Smartphone s1 = new Smartphone("2NSB4", "Apple");
+        Smartphone s2 = new Smartphone("2NSB4", "Apple");
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+    }
+}
