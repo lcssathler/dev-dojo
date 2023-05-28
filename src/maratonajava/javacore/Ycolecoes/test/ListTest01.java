35d4d76daf41c53f3769f71d2160cab65bc31240
Lucas Sathler <lcssathler@gmail.com>
2023-05-28 12:08:04 -0300

    Aula 168 - Coleções pt 08 - List pt 03

--- src/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ src/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -12,12 +12,27 @@ public class ListTest01 {
         names2.add("Aguiar");
         names2.add("Lucas");
         names1.addAll(names2);
+        names1.clear();
+        System.out.println(names1.toString());
 
         for (int i = 0; i < names1.size(); i++) {
             System.out.println(names1.get(i));
         }
+        System.out.println("-=".repeat(10));
 
         List<Integer> nums = new ArrayList();
-        nums.add(1);
+        nums.add(245);
+        nums.add(1123);
+        nums.add(111);
+        nums.add(0, 20000);
+        System.out.println(nums);
+
+        System.out.println(nums.indexOf(999));
+
+        System.out.println(nums.contains(123));
+        System.out.println(nums.contains(111));
+
+        System.out.println(nums.get(3));
+        System.out.println(nums.get(20));
     }
 }
