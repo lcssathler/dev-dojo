6b8997ebd293058ef67e866ca4bed0a99cbdb66c
Lucas Sathler <lcssathler@gmail.com>
2023-06-06 18:50:30 -0300

    Aula 185 - Generics pt 03 - Wildcard pt 02

--- src/maratonajava/javacore/Zgenerics/test/WildCardTest02.java
+++ src/maratonajava/javacore/Zgenerics/test/WildCardTest02.java
@@ -1,20 +1,30 @@
 package maratonajava.javacore.Zgenerics.test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WildCardTest02 {
     public static void main(String[] args) {
         List<Cat> cats = List.of(new Cat(), new Cat());
         List<Dog> dogs = List.of(new Dog(), new Dog());
-        List<Animal> animals = List.of(new Cat(), new Dog());
-//        printConsult(cats);
-//        printConsult(dogs);
-        printConsult(animals);
+        printConsultExtends(cats);
+        printConsultExtends(dogs);
+
+        List<Animal> animals = new ArrayList();
+        printConsultSuper(animals);
     }
 
-    public static void printConsult(List<Animal> animals) {
+    public static void printConsultExtends(List<? extends Animal> animals) {
         for (Animal animal : animals) {
             animal.consult();
         }
+    }
+
+    public static void printConsultSuper(List<? super Animal> animals) {
+        for (Object animal : animals) {
+            System.out.println(animal);
+        }
+        animals.add(new Dog());
+        animals.add(new Cat());
     }
 }
