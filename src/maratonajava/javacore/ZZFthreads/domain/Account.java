aa8e92ce358c3b7a432531f75b61f22f574f51a9
Lucas Sathler <lcssathler@gmail.com>
2023-07-01 16:15:23 -0300

    225 - Threads pt 06 - Sincronismo de thread pt 02

---
+++ src/maratonajava/javacore/ZZFthreads/domain/Account.java
@@ -0,0 +1,15 @@
+package maratonajava.javacore.ZZFthreads.domain;
+
+public class Account {
+    private int balance = 50;
+
+    public void withdrawal(int amount) {
+        this.balance = this.balance - amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
+
+
---
+++ src/maratonajava/javacore/ZZFthreads/test/ThreadsAccountTest01.java
@@ -0,0 +1,40 @@
+package maratonajava.javacore.ZZFthreads.test;
+
+import maratonajava.javacore.ZZFthreads.domain.Account;
+
+public class ThreadsAccountTest01 implements Runnable{
+    private Account account = new Account();
+
+
+    public static void main(String[] args) {
+        ThreadsAccountTest01 accountTest01 = new ThreadsAccountTest01();
+        Thread thread1 = new Thread(accountTest01, "Thread-1");
+        Thread thread2 = new Thread(accountTest01, "Thread-2");
+        thread1.start();
+        thread2.start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            withdrawal(10);
+            if (account.getBalance() < 0) {
+                System.out.println("ERROR! NEGATIVE BALANCE!");
+            }
+        }
+    }
+
+    private void withdrawal(int amount) {
+        if (account.getBalance() >= amount) {
+            System.out.println(getName() + " is going to withdrawal money");
+            account.withdrawal(amount);
+            System.out.println(getName() + " finished withdrawing money. New balance: " + account.getBalance());
+        } else {
+            System.out.println(getName()+", insufficient balance to withdraw!");
+        }
+    }
+
+    private static String getName() {
+        return Thread.currentThread().getName();
+    }
+}
