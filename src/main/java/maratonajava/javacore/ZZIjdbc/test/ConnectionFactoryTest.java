e630f575edb9087076c717d04d88c67033ee7646
Lucas Sathler <lcssathler@gmail.com>
2023-07-28 18:59:07 -0300

    256 - JDBC pt 05 - Adicionando dependência e conectando com banco

---
+++ src/main/java/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest.java
@@ -0,0 +1,9 @@
+package maratonajava.javacore.ZZIjdbc.test;
+
+import maratonajava.javacore.ZZIjdbc.connection.ConnectionFactory;
+
+public class ConnectionFactoryTest {
+    public static void main(String[] args) {
+        System.out.println(ConnectionFactory.getConnection());
+    }
+}
