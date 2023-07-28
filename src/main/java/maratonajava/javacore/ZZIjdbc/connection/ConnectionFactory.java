ebbe8dd4011a6f8b312ab26ca5c4ad6821175575
Lucas Sathler <lcssathler@gmail.com>
2023-07-28 18:57:57 -0300

    256 - JDBC pt 05 - Adicionando dependência e conectando com banco

--- src/main/java/maratonajava/javacore/ZZIjdbc/connection/ConnectionFactory.java
+++ src/main/java/maratonajava/javacore/ZZIjdbc/connection/ConnectionFactory.java
@@ -1,4 +1,20 @@
 package maratonajava.javacore.ZZIjdbc.connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
+    public static Connection getConnection() {
+        String url = "jdbc:mysql://localhost:3306/anime_store";
+        String username = "root";
+        String password = "root";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            return connection;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
